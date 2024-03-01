2a8807989f5c3f5941adee76d48f645f436e1af3
Nguyen Dac Tuan Thanh <nguyendactuanthanh@Nguyens-MBP.hub>
2024-03-01 16:23:25 +1100

    first commit

---
+++ index1.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping Cart</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="style1.css">
+   
+</head>
+<body>
+  <section id="header">
+    <a href="#"> <img src="img/logo.png" class="logo" alt=""></a>
+    
+
+    <div>
+        <ul id="navbar">
+            <li><a class="active" href="index.php">Home</a></li>
+            <li><a href="shop.php">Shop</a></li>
+            <li><a href="blog.php">Blog</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="cart.php"><i class="fa-solid fa-cart-shopping"></i></a></li>
+
+        </ul>
+    </div>
+  </section>
+  
+ <section id="hero">
+   <h4> Trade-in-offer</h4>
+   <h2> Super value deals</h2>
+   <h1> On all products</h1>
+   <p> Save more with coupons & up to 70% off!</p>
+   <button>Shop Now</button>
+
+</section>
+
+<section id="feature" class="section-p1">
+  <div class="fe-box">
+    <img src="img/features/f1.png" alt="">
+    <h6>Free shipping</h6>
+  </div>
+  <div class="fe-box">
+    <img src="img/features/f2.png" alt="">
+    <h6>Online Order</h6>
+  </div>
+  <div class="fe-box">
+    <img src="img/features/f3.png" alt="">
+    <h6>Save Money</h6>
+  </div>
+  <div class="fe-box">
+    <img src="img/features/f4.png" alt="">
+    <h6>Promotions</h6>
+  </div>
+  <div class="fe-box">
+    <img src="img/features/f5.png" alt="">
+    <h6>Happy Sell</h6>
+  </div>
+  <div class="fe-box">
+    <img src="img/features/f6.png" alt="">
+    <h6>24/7 Hours Support</h6>
+  </div>
+</section>
+
+<section id="product1">
+   <h2>Featured Products</h2>
+   <p> Summer Collection New Mordern Design</p>
+   <div class="pro-cotainer">
+     <div class="pro">
+        <img src="img/products/f1.jpg" alt="">
+        <div class="des">
+           <span>Adidas</span>
+           <h5>Cartoon T-Shirts</h5>
+           <div class="star">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+           </div>
+           <h4>$78</h4>
+        </div>
+        <a href="#"><i class="fa fa-cart-shopping "></i></a>
+     </div>
+
+     <div class="pro">
+        <img src="img/products/f2.jpg" alt="">
+        <div class="des">
+           <span>Adidas</span>
+           <h5>Cartoon T-Shirts</h5>
+           <div class="star">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+           </div>
+           <h4>$78</h4>
+        </div>
+        <a href="#"><i class="fa fa-cart-shopping "></i></a>
+     </div>
+
+     <div class="pro">
+        <img src="img/products/f3.jpg" alt="">
+        <div class="des">
+           <span>Adidas</span>
+           <h5>Cartoon T-Shirts</h5>
+           <div class="star">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+           </div>
+           <h4>$78</h4>
+        </div>
+        <a href="#"><i class="fa fa-cart-shopping "></i></a>
+     </div>
+
+     <div class="pro">
+        <img src="img/products/f4.jpg" alt="">
+        <div class="des">
+           <span>Adidas</span>
+           <h5>Cartoon T-Shirts</h5>
+           <div class="star">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+           </div>
+           <h4>$78</h4>
+        </div>
+        <a href="#"><i class="fa fa-cart-shopping "></i></a>
+     </div>
+
+     <div class="pro">
+        <img src="img/products/f5.jpg" alt="">
+        <div class="des">
+           <span>Adidas</span>
+           <h5>Cartoon T-Shirts</h5>
+           <div class="star">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+           </div>
+           <h4>$78</h4>
+        </div>
+        <a href="#"><i class="fa fa-cart-shopping "></i></a>
+     </div>
+
+     <div class="pro">
+        <img src="img/products/f6.jpg" alt="">
+        <div class="des">
+           <span>Adidas</span>
+           <h5>Cartoon T-Shirts</h5>
+           <div class="star">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+           </div>
+           <h4>$78</h4>
+        </div>
+        <a href="#"><i class="fa fa-cart-shopping "></i></a>
+     </div>
+
+     <div class="pro">
+        <img src="img/products/f7.jpg" alt="">
+        <div class="des">
+           <span>Adidas</span>
+           <h5>Cartoon T-Shirts</h5>
+           <div class="star">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+           </div>
+           <h4>$78</h4>
+        </div>
+        <a href="#"><i class="fa fa-cart-shopping "></i></a>
+     </div>
+
+     <div class="pro">
+        <img src="img/products/f8.jpg" alt="">
+        <div class="des">
+           <span>Adidas</span>
+           <h5>Cartoon T-Shirts</h5>
+           <div class="star">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+           </div>
+           <h4>$78</h4>
+        </div>
+        <a href="#"><i class="fa fa-cart-shopping "></i></a>
+     </div>
+   </div>
+</section>
+
+</div>
+<script src="script.js"></script>
+</body>
+</html>
